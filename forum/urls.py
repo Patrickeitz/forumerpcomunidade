4bc76287a7d24cc2d4c1bb68c5276c1640428f11
Patrickeitz <patrickeitz@yahoo.com.br>
2025-11-17 00:28:38 -0300

    Update urls.py

--- forum/urls.py
+++ forum/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('minha-conta/', views.minha_conta, name='minha_conta'),
     path('excluir-conta/', views.excluir_conta, name='excluir_conta'),
     path('register/', views.register, name='register'),
+    path('comentario/<int:comentario_id>/editar/', views.editar_comentario, name='editar_comentario'),
+    path('comentario/<int:comentario_id>/excluir/', views.excluir_comentario, name='excluir_comentario'),
 ]
